535403e130f00b44f3767278fba73373f5134312
Maryam Dabaghchian <maryam81609@users.noreply.github.com>
2017-06-07 09:36:31 -0600

    update debuging code

--- comm_tests/wallet_comm.erl
+++ comm_tests/wallet_comm.erl
@@ -26,31 +26,25 @@ check(Config) ->
     Clusters = proplists:get_value(clusters, Config),
     [Node1, Node2, Node3 | _Nodes] =  [ hd(Cluster)|| Cluster <- Clusters ],
 
-    main_test(Node1, Node2, Node3),
-    pass.
+    main_test(Node1, Node2, Node3).
 
 main_test(Node1, Node2, Node3) ->
     Key = wallet_key,
     Wallet = {Key, antidote_crdt_counter, bucket},
 
-    ?DEBUG_LOG(io_lib:format("~p", [node()])),
-
     TestNode = list_to_atom(os:getenv("TESTNODE")),
     true = rpc:call(TestNode, lists, member, [commander, erlang:registered()]),
-%%    true = lists:member(commander, erlang:registered()),
+
     %%% Specify invariant objects
-    %%comm_test:objects(?MODULE, [Wallet]),
+    comm_test:objects(?MODULE, [Wallet]),
 
     {_Re, CT} = comm_test:event(?MODULE, [2, Node1, ignore, [Wallet, 26500]]),
 
-    if
-        ?DEBUG ->
-            ct:print("~n^^^^^^^^^^CT: ~w^^^^^^^^^^^~n", [dict:to_list(CT)]);
-        true -> skip
-    end,
-    [?DEBUG_LOG(io_lib:format("Wallet Val on node: ~p ==> ~p", [Node, get_val(Node, Wallet, CT)]))
+    ?DEBUG_LOG(io_lib:format("~n^^^^^^^^^^CT: ~w^^^^^^^^^^^~n", [dict:to_list(CT)])),
+    [?DEBUG_LOG(io_lib:format("Wallet Val on node: ~p ==> ~p", [Node, wallet:get_val(Node, Wallet, CT)]))
         || Node <- [Node1, Node2, Node3]],
-    [?assertEqual(get_val(Node, Wallet, CT), 26500) || Node <- [Node1, Node2, Node3]],
+
+    [?assertEqual(wallet:get_val(Node, Wallet, CT), 26500) || Node <- [Node1, Node2, Node3]],
 
     CT1 = dc1_txns(Node1, Wallet, CT),
     CT2 = dc2_txns(Node2, Wallet, CT),
@@ -60,28 +54,18 @@ main_test(Node1, Node2, Node3) ->
         CT1,
         dict:merge(fun(_K, T1, T2) -> max(T1, T2) end, CT2, CT3)),
 
-    Vals = [get_val(Node, Wallet, Time) || Node <- [Node1, Node2, Node3]],
+    Vals = [wallet:get_val(Node, Wallet, Time) || Node <- [Node1, Node2, Node3]],
     ct:print("Vals: ~w", [Vals]),
 
-    if
-        ?DEBUG ->
-            ct:print("CT1:~w~nCT2:~w~nCT3:~w~n~nTime:~w~n",
-                [dict:to_list(CT1), dict:to_list(CT2), dict:to_list(CT3), dict:to_list(Time)]),
-            ct:print("Val on node 1: ~p", [get_val(Node1, Wallet, Time)]),
-            %%  Quiescence_val = lists:usort(Vals),
-            %%  ?assertMatch(Quiescence_val, [hd(Vals)]),
-            ct:print("Cookie: ~p, Node: ~p", [erlang:get_cookie(), node()]),
-            ct:print("Self: ~p~n commander: ~p", [self(), whereis(commander)]);
-        true -> skip
-    end,
+    ?DEBUG_LOG(io_lib:format("CT1:~w~nCT2:~w~nCT3:~w~n~nTime:~w~n",
+                [dict:to_list(CT1), dict:to_list(CT2), dict:to_list(CT3), dict:to_list(Time)])),
+    ?DEBUG_LOG(io_lib:format("Val on node 1: ~p", [wallet:get_val(Node1, Wallet, Time)])),
+    Quiescence_val = lists:usort(Vals),
+    ?assertMatch(Quiescence_val, [hd(Vals)]),
+    ?DEBUG_LOG(io_lib:format("Cookie: ~p, Node: ~p", [erlang:get_cookie(), node()])),
+    ?DEBUG_LOG(io_lib:format("comm test Self: ~p   commander: ~p", [self(), whereis(commander)])),
 
     pass.
-
-get_val(Node, Wallet, Clock) ->
-    {ok, Tx} = rpc:call(Node, antidote, start_transaction, [Clock, []]),
-    {ok, [Res]} = rpc:call(Node, antidote, read_objects, [[Wallet], Tx]),
-    {ok, _CT1} = rpc:call(Node, antidote, commit_transaction, [Tx]),
-    Res.
 
 dc1_txns(Node, Wallet, ST) ->
     {_R1, _CT1} = comm_test:event(?MODULE, [1, Node, ST, [Wallet, 200]]),
@@ -100,7 +84,7 @@ dc2_txns(Node, Wallet, ST) ->
     {_R1, _CT1} = comm_test:event(?MODULE, [1, Node, ST, [Wallet, 800]]),
     {_R2, _CT2} = comm_test:event(?MODULE, [1, Node, ST, [Wallet, 500]]),
     {_R3, _CT3} = comm_test:event(?MODULE, [1, Node, ST, [Wallet, 600]]),
-    {_R4, _CT4} = comm_test:event(?MODULE, [2, Node, ST, [Wallet, 5000]]),
+    {_R4, _CT4} = comm_test:event(?MODULE, [2, Node, ST, [Wallet, 100]]),%%5000
     {_R5, _CT5} = comm_test:event(?MODULE, [1, Node, ST, [Wallet, 900]]),
     {_R6, _CT6} = comm_test:event(?MODULE, [1, Node, ST, [Wallet, 1200]]),
     {_R7, _CT7} = comm_test:event(?MODULE, [1, Node, ST, [Wallet, 2000]]),
@@ -122,7 +106,6 @@ dc3_txns(Node, Wallet, ST) ->
     {_R9, CT9} = comm_test:event(?MODULE, [1, Node, ST, [Wallet, 1400]]),
     CT9.
 
-
 %%%====================================
 %%% Callbacks
 %%%====================================
@@ -131,15 +114,8 @@ handle_event([1, Node, ST, AppArgs]) ->
     {Res1, {_Tx1, CT1}} = wallet:debit(Node, Wallet, N, ST),
     {ok, [Res], _CT} = rpc:call(Node, antidote, read_objects, [ignore, [], [Wallet]]),
 
-    if
-        ?DEBUG ->
-            ct:print("~n:::::::::::::::::: DEBIT EVENT (1) FOR ::::::::::::::::::::
-            node: ~w ~n ~p ~n WITH RESULT: ~p [~p]", [Node, AppArgs, Res, Res1]);
-        true -> skip
-    end,
-
-%%    io:format("~n:::::::::::::::::: DEBIT EVENT (1) FOR ::::::::::::::::::::
-%% node: ~w ~n ~p ~n WITH RESULT: ~p [~p]", [Node, AppArgs, Res, Res1]),
+    ?DEBUG_LOG(io_lib:format("~n:::::::::::::::::: DEBIT EVENT (1) FOR ::::::::::::::::::::
+            node: ~w ~n ~p ~n WITH RESULT: ~p [~p]", [Node, AppArgs, Res, Res1])),
 
     {Res1, CT1};
 
@@ -148,12 +124,8 @@ handle_event([2, Node, ST, AppArgs]) ->
     {Res2, {_Tx2, CT2}} = wallet:credit(Node, Wallet, N, ST),
     {ok, [Res], _CT} = rpc:call(Node, antidote, read_objects, [ignore, [], [Wallet]]),
 
-    if
-        ?DEBUG ->
-            ct:print("~n:::::::::::::::::: CREDIT EVENT (2) FOR ::::::::::::::::::::
-            node: ~w ~p~nWITH RESULT:~p [~p]", [Node, AppArgs, Res, Res2]);
-        true -> skip
-    end,
+    ?DEBUG_LOG(io_lib:format("~n:::::::::::::::::: CREDIT EVENT (2) FOR ::::::::::::::::::::
+            node: ~w ~p~nWITH RESULT:~p [~p]", [Node, AppArgs, Res, Res2])),
 
     {Res2, CT2};
 
@@ -163,8 +135,7 @@ handle_event([3, Node, ST, AppArgs]) ->
     CT.
 
 handle_object_invariant(Node, [Wallet]) ->
-    WalletVal =  get_val(Node, Wallet, ignore),
+    WalletVal =  wallet:get_val(Node, Wallet, ignore),
     ct:print("~nWallet value:~p~n", [WalletVal]),
-%%    io:format("~nWallet value:~p~n", [WalletVal]),
     ?assert(WalletVal >= 0),
     true.
