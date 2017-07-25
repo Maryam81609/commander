9c068271260b514a0dd52a6dd4383fbcebc91b7f
Maryam Dabaghchian <maryam81609@users.noreply.github.com>
2017-07-25 11:45:10 -0600

    removed DB nodes from application API

--- comm_tests/b2b_orders_comm.erl
+++ comm_tests/b2b_orders_comm.erl
@@ -17,12 +17,14 @@
 -define(BUDGET_UPDATE3, 30000).
 
 check(Config) ->
-    Clusters = proplists:get_value(clusters, Config),
-    [Node1, Node2, Node3 | _Nodes] =  [ hd(Cluster)|| Cluster <- Clusters ],
+    ct:print("Entered check!"),
+    [Node1, Node2, Node3] = proplists:get_value(sut_nodes, Config),
 
-    main_test(Node1, Node2, Node3).
+    main_test([Node1, Node2, Node3]),
 
-main_test(Node1, Node2, Node3) ->
+    pass.
+
+main_test([Node1, Node2, Node3]) ->
     %%% init budget for every store on every DC
     {ok, CTInit} = comm_test:event(?MODULE, [1, Node1, ignore, [?INITBUDGET]]),
 
@@ -31,7 +33,7 @@ main_test(Node1, Node2, Node3) ->
     CT3 = dc3_txns(Node3, [], [CTInit]),
 
     %%% Specify invariant objects
-    BudgetObjs = b2b_orders:create_budget_obj(all),
+    BudgetObjs = rpc:call(Node1, b2b_orders, create_budget_obj, [all]),
     comm_test:objects(?MODULE, BudgetObjs),
 
     Time =
@@ -43,18 +45,18 @@ main_test(Node1, Node2, Node3) ->
                           end, CT2, CT3)),
 
     [BudgetObj] = BudgetObjs,
-    Vals = [b2b_orders:get_val(Node, BudgetObj, Time) || Node <- [Node1, Node2, Node3]],
+    Vals = [rpc:call(Node, b2b_orders, get_val, [BudgetObj, Time]) || Node <- [Node1, Node2, Node3]],
 
     Quiescence_val = lists:usort(Vals),
     ?assertMatch(Quiescence_val, [hd(Vals)]),
     pass.
 
 dc1_txns(Node, _Orders, [STInit]) ->
-    Order1 = {b2b_orders:get_object_of(dress), 50, "Jan. 29"},
-    Order2 = {b2b_orders:get_object_of(pants), 50, "Feb. 10"},
-    Order3 = {b2b_orders:get_object_of(shirt), 50, "Feb. 15"},
-    Order4 = {b2b_orders:get_object_of(shirt), 150, "March. 15"},
-    Order5 = {b2b_orders:get_object_of(pants), 50, "Aug. 23"},
+    Order1 = {dress, 50, "Jan. 29"},
+    Order2 = {pants, 50, "Feb. 10"},
+    Order3 = {shirt, 50, "Feb. 15"},
+    Order4 = {shirt, 150, "March. 15"},
+    Order5 = {pants, 50, "Aug. 23"},
 
     {_Res1, CT1} = comm_test:event(?MODULE, [3, Node, STInit, [{store1, [Order1]}]]),
     {_Res2, CT2} = comm_test:event(?MODULE, [3, Node, STInit, [{store1, [Order2]}]]),
@@ -65,11 +67,11 @@ dc1_txns(Node, _Orders, [STInit]) ->
     vc_max(vc_max(CT1, CT2), vc_max(vc_max(CT4, CT3), CT5)).
 
 dc2_txns(Node, _Orders, [STInit]) ->
-    Order1 = {b2b_orders:get_object_of(shirt), 80, "Feb. 19"},
-    Order2 = {b2b_orders:get_object_of(pants), 50, "Feb. 28"},
-    Order3 = {b2b_orders:get_object_of(shirt), 150, "April. 19"},
-    Order4 = {b2b_orders:get_object_of(dress), 60, "March. 29"},
-    Order5 = {b2b_orders:get_object_of(pants), 50, "Jan. 28"},
+    Order1 = {shirt, 80, "Feb. 19"},
+    Order2 = {pants, 50, "Feb. 28"},
+    Order3 = {shirt, 150, "April. 19"},
+    Order4 = {dress, 60, "March. 29"},
+    Order5 = {pants, 50, "Jan. 28"},
 
     {_Res1, CT1} = comm_test:event(?MODULE, [3, Node, STInit, [{store1, [Order1]}]]),
     {_Res2, CT2} = comm_test:event(?MODULE, [3, Node, STInit, [{store1, [Order2]}]]),
@@ -80,12 +82,12 @@ dc2_txns(Node, _Orders, [STInit]) ->
     vc_max(vc_max(CT1, CT2), vc_max(vc_max(CT4, CT3), CT5)).
 
 dc3_txns(Node, _Orders, [STInit]) ->
-    Order1 = {b2b_orders:get_object_of(dress), 50, "March. 17"},
-    Order2 = {b2b_orders:get_object_of(pants), 30, "Feb. 01"},
-    Order3 = {b2b_orders:get_object_of(shirt), 50, "April. 19"},
-    Order4 = {b2b_orders:get_object_of(shirt), 75, "June. 21"},
-    Order5 = {b2b_orders:get_object_of(pants), 40, "Sept. 11"},
-    Order6 = {b2b_orders:get_object_of(pants), 30, "Feb. 01"},
+    Order1 = {dress, 50, "March. 17"},
+    Order2 = {pants, 30, "Feb. 01"},
+    Order3 = {shirt, 50, "April. 19"},
+    Order4 = {shirt, 75, "June. 21"},
+    Order5 = {pants, 40, "Sept. 11"},
+    Order6 = {pants, 30, "Feb. 01"},
 
     {_Res, _CT} = comm_test:event(?MODULE, [1, Node, STInit, [30000]]),
     {_Res1, CT1} = comm_test:event(?MODULE, [3, Node, STInit, [{store1, [Order1]}]]),
@@ -103,18 +105,18 @@ dc3_txns(Node, _Orders, [STInit]) ->
 %%% init store budgets
 handle_event([1, Node, ST, AppArgs]) ->
     [Val] = AppArgs,
-    {ok, CT1} = b2b_orders:init_budget(Node, ST, Val),
+    {ok, CT1} = rpc:call(Node, b2b_orders, init_budget, [ST, Val]),
     {ok, CT1};
 
 %%% init stock availability
 handle_event([2, Node, ST, AppArgs]) ->
     [Count] = AppArgs,
-    {ok, CT1} = b2b_orders:init_availability(Node, ST, Count),
+    {ok, CT1} = rpc:call(Node, b2b_orders, init_availability, [ST, Count]),
     {ok, CT1};
 
 handle_event([3, Node, ST, AppArgs]) ->
     [{StoreId, Orders}] = AppArgs,
-    {_Res, {_TxId, CT1}} = b2b_orders:place_order(Node, ST, {StoreId, Orders}),
+    {_Res, {_TxId, CT1}} = rpc:call(Node, b2b_orders, place_order, [ST, {StoreId, Orders}]),
     {ok, CT1}.
 
 
@@ -123,7 +125,7 @@ handle_event([3, Node, ST, AppArgs]) ->
 %%%=======================================
 handle_object_invariant(Node, [BudgetObj]) ->
     {Key, _Type, _} = BudgetObj,
-    Budget =  b2b_orders:get_val(Node, BudgetObj, ignore),
+    Budget =  rpc:call(Node, b2b_orders, get_val, [BudgetObj, ignore]),
     ct:print("~nBudget for Store: ~w on node: ~w became: ~p~n", [Key, Node, Budget]),
     ?assert(Budget >= 0),
     true.
